6374d95581ef37e856947b077f9cb0e40b26a208
Sascha Gerspach <sascha.gerspach@gmx.de>
2026-02-22 15:52:15 +0100

    feat(auth): add logout action in boards page

--- frontend/src/app/features/boards/pages/boards-page/boards-page.component.ts
+++ frontend/src/app/features/boards/pages/boards-page/boards-page.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterModule, Router } from "@angular/router";
+import { AuthService } from '../../../../core/auth/auth.service';
 
 @Component({
   selector: 'app-boards-page',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './boards-page.component.html',
   styleUrl: './boards-page.component.scss'
 })
 export class BoardsPageComponent {
+  private readonly auth = inject(AuthService);
+  private readonly router = inject(Router);
 
+  logout(): void {
+    this.auth.logout();
+    this.router.navigate(['login']);
+  }
 }
