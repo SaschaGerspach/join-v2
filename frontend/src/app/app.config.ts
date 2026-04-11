196eec2a7f1c8122875d08c4a42bc3237efab025
Sascha Gerspach <sascha.gerspach@gmx.de>
2026-04-11 16:21:38 +0200

    feat(auth): add CSRF interceptor for Django session auth

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,12 +1,12 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
-
+import { csrfInterceptor } from './core/interceptors/csrf.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: 
-    [provideRouter(routes),
-      provideHttpClient(),
-    ],
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([csrfInterceptor])),
+  ],
 };
