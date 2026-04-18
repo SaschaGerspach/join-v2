bc88d695296ece34b82c2fbe1cb6e60201b7edba
Sascha Gerspach <sascha.gerspach@gmx.de>
2026-04-18 19:50:43 +0200

    fix(permissions): add missing is_board_owner function

--- backend/boards_api/permissions.py
+++ backend/boards_api/permissions.py
@@ -1,2 +1,6 @@
 def can_access_board(board, user):
     return board.created_by_id == user.id or board.members.filter(user=user).exists()
+
+
+def is_board_owner(board, user):
+    return board.created_by_id == user.id
