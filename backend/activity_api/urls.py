7d329775d8b5ddb5bf4873865820bd1dc9a3d675
Sascha Gerspach <sascha.gerspach@gmx.de>
2026-04-20 10:25:22 +0200

    chore: add app_name to notifications_api and activity_api URL configs

--- backend/activity_api/urls.py
+++ backend/activity_api/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "activity_api"
+
 urlpatterns = [
     path("", views.activity_list, name="activity-list"),
 ]
--- backend/notifications_api/urls.py
+++ backend/notifications_api/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "notifications_api"
+
 urlpatterns = [
     path("", views.notification_list, name="notification-list"),
     path("<int:pk>/read/", views.notification_read, name="notification-read"),
