ac098bf8b7b4fb20bab75497955767ed475565e9
Sascha Gerspach <sascha.gerspach@gmx.de>
2026-04-21 09:28:09 +0200

    feat: add DB connection pooling (CONN_MAX_AGE) and horizontal scaling (replicas)

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -123,6 +123,8 @@ if _db_name:
             'PASSWORD': os.environ.get('POSTGRES_PASSWORD', ''),
             'HOST': os.environ.get('POSTGRES_HOST', 'db'),
             'PORT': os.environ.get('POSTGRES_PORT', '5432'),
+            'CONN_MAX_AGE': int(os.environ.get('DB_CONN_MAX_AGE', '600')),
+            'CONN_HEALTH_CHECKS': True,
         }
     }
 elif DEBUG:
