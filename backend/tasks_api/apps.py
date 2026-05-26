7fca207444c7cf4e34119607795e4ca8520b0f05
Sascha Gerspach <sascha.gerspach@gmx.de>
2026-05-26 14:02:32 +0200

    feat: post_delete signal to clean up orphaned attachment files from storage

--- backend/tasks_api/apps.py
+++ backend/tasks_api/apps.py
@@ -3,3 +3,14 @@ from django.apps import AppConfig
 
 class TasksApiConfig(AppConfig):
     name = 'tasks_api'
+
+    def ready(self):
+        from django.db.models.signals import post_delete
+        from .models import Attachment
+
+        post_delete.connect(_delete_attachment_file, sender=Attachment)
+
+
+def _delete_attachment_file(sender, instance, **kwargs):
+    if instance.file:
+        instance.file.delete(save=False)
